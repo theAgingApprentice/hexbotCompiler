271f5c735bf68fbc36845ff582542bd2ac08a5a4
theAgingApprentice <TheAgingApprentice@protonmail.com>
2021-11-24 17:21:33 -0500

    Wrong icon in Doxygen navbar Fixes #6

--- src/hexflow.h
+++ src/hexflow.h
@@ -1,14 +1,3 @@
-/** @file hexflow.h
- * A command line utility for making Hexbot flows.
- * Hexflow is a command line utitlity that outputs flows that can be used to 
- * issue leg movement commands to the  hexbot robot via MQTT messages.   
- * 
- * # Getting started
- * To get your C++ envionment up and running in Visual Studio Code please
- * check out the link for 
- * [MacOsx](https://code.visualstudio.com/docs/cpp/config-clang-mac)]or this 
- * link for [Windows](https://code.visualstudio.com/docs/cpp/config-mingw).
- ******************************************************************************/
 #ifndef HEXFLOW_H
 #define HEXFLOW_H
 
