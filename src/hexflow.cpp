0fb0944405fe67448667200d4a3265462d8a3ce5
theAgingApprentice <TheAgingApprentice@protonmail.com>
2021-11-24 15:42:29 -0500

    Fix Doxygen for this project Fixes #7

--- src/hexflow.cpp
+++ src/hexflow.cpp
@@ -1,26 +1,10 @@
-#include <iostream> // C++ standard library. Stream support.
-//#include <vector> // C++ standard library. Dynamic array suppport.
-//#include <string> // Handy character array tools.
+#include "hexflow.h"
 
 using namespace std; // Scope identifiers like functions and variables etc. 
 
-/**
- * hexflow is a command line utitlity that outputs flows that can be used to 
- * issue leg movement commands to the  hexbot robot via MQTT messages.   
- * 
- * # Getting started
- * To get your C++ envionment up and running in Visual Studio Code please
- * check out the link for 
- * [MacOsx](https://code.visualstudio.com/docs/cpp/config-clang-mac)]or this 
- * link for [Windows](https://code.visualstudio.com/docs/cpp/config-mingw).
- * 
- * @param argc Count of all command line elements including the program name.
- * @param argv Array of character strings containing each command line argument.
- * @param env_var_ptr Array of characters containing all ebvironment variables.
- * @return 0 unconditionally.
- ******************************************************************************/
 int main(int argc, char** argv, char **env_var_ptr)
 {
+   printf("<main> hexflow is a program that creates flows for hexbot legs.\n");
    printf("<main> Program name is %s\n", argv[0]);
    printf("<main> Number of arguments supplied is %d\n", argc);
    printf("<main> Comand line arguments:\n");
---
+++ src/hexflow.h
@@ -0,0 +1,25 @@
+/** @file */ 
+#ifndef HEXFLOW_H
+#define HEXFLOW_H
+
+#include <iostream> // C++ standard library. Stream support.
+// #include <vector> // C++ standard library. Dynamic array suppport.
+// #include <string> // Handy character array tools.
+
+/**
+ * hexflow is a command line utitlity that outputs flows that can be used to 
+ * issue leg movement commands to the  hexbot robot via MQTT messages.   
+ * 
+ * # Getting started
+ * To get your C++ envionment up and running in Visual Studio Code please
+ * check out the link for 
+ * [MacOsx](https://code.visualstudio.com/docs/cpp/config-clang-mac)]or this 
+ * link for [Windows](https://code.visualstudio.com/docs/cpp/config-mingw).
+ * 
+ * @param argc Count of all command line elements including the program name.
+ * @param argv Array of character strings containing each command line argument.
+ * @param env_var_ptr Array of characters containing all ebvironment variables.
+ * @return 0 unconditionally.
+ ******************************************************************************/
+
+#endif
