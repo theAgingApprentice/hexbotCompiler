abe3a9142fa55afc3f4f7e133c594ffeac099b29
theAgingApprentice <TheAgingApprentice@protonmail.com>
2022-01-12 20:36:10 -0500

    Implement menuing system Fixes #14

--- src/hexflow.h
+++ src/hexflow.h
@@ -2,7 +2,9 @@
 #define HEXFLOW_H
 
 #include <iostream> // C++ standard library. Stream support.
+//#include <stdio.h> 
 // #include <vector> // C++ standard library. Dynamic array suppport.
 // #include <string> // Handy character array tools.
+bool appActive = true; // Flag controlling ending of execution of the app.
 
 #endif
